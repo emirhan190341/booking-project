21ae99c83e94babe4c4099ccda2867fc69e160d4
emirhanaricii <emirhan190341@gmail.com>
2023-12-19 14:37:21 +0300

    createBook method added

--- src/main/java/com/emirhanarici/bookingproject/service/BookService.java
+++ src/main/java/com/emirhanarici/bookingproject/service/BookService.java
@@ -1,4 +1,31 @@
 package com.emirhanarici.bookingproject.service;
 
+import com.emirhanarici.bookingproject.dto.BookDTO;
+import com.emirhanarici.bookingproject.model.Book;
+import com.emirhanarici.bookingproject.model.mapper.book.BookMapper;
+import com.emirhanarici.bookingproject.payload.request.book.BookCreateRequest;
+import com.emirhanarici.bookingproject.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    /**
+     * Creates a new book based on the provided request.
+     *
+     * @param request The request containing book information.
+     * @return A {@link BookDTO} representing the newly created book.
+     */
+    public BookDTO createBook(BookCreateRequest request) {
+
+        final Book bookEntityToBeSaved = BookMapper.mapForSaving(request);
+
+        return BookMapper.toDTO(bookRepository.save(bookEntityToBeSaved));
+    }
+
+
 }
