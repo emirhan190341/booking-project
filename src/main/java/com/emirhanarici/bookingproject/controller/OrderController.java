bafc7dea2d1352f17d8682691e718fa49fd440f8
emirhanaricii <emirhan190341@gmail.com>
2023-12-22 16:49:42 +0300

    revise ordercontroller

--- src/main/java/com/emirhanarici/bookingproject/controller/OrderController.java
+++ src/main/java/com/emirhanarici/bookingproject/controller/OrderController.java
@@ -1,6 +1,12 @@
 package com.emirhanarici.bookingproject.controller;
 
+import com.emirhanarici.bookingproject.dto.OrderDTO;
+import com.emirhanarici.bookingproject.model.mapper.order.OrderMapper;
+import com.emirhanarici.bookingproject.payload.request.order.CreateOrderRequest;
 import com.emirhanarici.bookingproject.payload.response.CustomResponse;
+import com.emirhanarici.bookingproject.payload.response.order.OrderCreatedResponse;
+import com.emirhanarici.bookingproject.service.OrderSaveService;
+import com.emirhanarici.bookingproject.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderSaveService orderSaveService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
