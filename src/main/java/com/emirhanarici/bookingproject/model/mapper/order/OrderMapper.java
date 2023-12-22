c49f36443cf65e66da03155eebdfd646efc0b4d6
emirhanaricii <emirhan190341@gmail.com>
2023-12-23 01:05:38 +0300

    Revise OrderMapper

--- src/main/java/com/emirhanarici/bookingproject/model/mapper/order/OrderMapper.java
+++ src/main/java/com/emirhanarici/bookingproject/model/mapper/order/OrderMapper.java
@@ -4,6 +4,7 @@ import com.emirhanarici.bookingproject.dto.OrderDTO;
 import com.emirhanarici.bookingproject.model.Order;
 import com.emirhanarici.bookingproject.model.mapper.user.UserMapper;
 import com.emirhanarici.bookingproject.payload.response.order.OrderCreatedResponse;
+import com.emirhanarici.bookingproject.payload.response.order.OrderGetResponse;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -26,6 +27,15 @@ public class OrderMapper {
                 .orderItems(orderDTO.getOrderItems())
                 .user(orderDTO.getUser())
                 .createdAt(orderDTO.getCreatedAt())
+                .build();
+    }
+
+    public static OrderGetResponse toGetResponse(OrderDTO source) {
+        return OrderGetResponse.builder()
+                .id(source.getId())
+                .user(source.getUser())
+                .orderItems(source.getOrderItems())
+                .createdAt(source.getCreatedAt())
                 .build();
     }
 
