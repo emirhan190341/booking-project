25af354cf9b49bcca81e3ea1da7701fa7f465466
emirhanaricii <emirhan190341@gmail.com>
2023-12-23 16:46:38 +0300

    new changes added

--- src/main/java/com/emirhanarici/bookingproject/model/mapper/order/OrderMapper.java
+++ src/main/java/com/emirhanarici/bookingproject/model/mapper/order/OrderMapper.java
@@ -3,9 +3,13 @@ package com.emirhanarici.bookingproject.model.mapper.order;
 import com.emirhanarici.bookingproject.dto.OrderDTO;
 import com.emirhanarici.bookingproject.model.Order;
 import com.emirhanarici.bookingproject.model.mapper.user.UserMapper;
+import com.emirhanarici.bookingproject.payload.response.CustomPageResponse;
 import com.emirhanarici.bookingproject.payload.response.order.OrderCreatedResponse;
+import com.emirhanarici.bookingproject.payload.response.order.OrderGetBetweenDatesResponse;
+import com.emirhanarici.bookingproject.payload.response.order.OrderGetByCustomerResponse;
 import com.emirhanarici.bookingproject.payload.response.order.OrderGetResponse;
 import lombok.experimental.UtilityClass;
+import org.springframework.data.domain.Page;
 
 @UtilityClass
 public class OrderMapper {
@@ -39,8 +43,32 @@ public class OrderMapper {
                 .build();
     }
 
+    public static OrderGetByCustomerResponse toGetByCustomerResponse(OrderDTO source) {
+        return OrderGetByCustomerResponse.builder()
+                .id(source.getId())
+                .user(source.getUser())
+                .orderItems(source.getOrderItems())
+                .createdAt(source.getCreatedAt())
+                .build();
+    }
 
 
+    public static CustomPageResponse<OrderGetByCustomerResponse> toGetByCustomerResponse(Page<OrderDTO> sources) {
+        return CustomPageResponse.of(sources.map(OrderMapper::toGetByCustomerResponse));
+    }
+
+    public static OrderGetBetweenDatesResponse toGetBetweenDatesResponse(OrderDTO source) {
+        return OrderGetBetweenDatesResponse.builder()
+                .id(source.getId())
+                .user(source.getUser())
+                .orderItems(source.getOrderItems())
+                .createdAt(source.getCreatedAt())
+                .build();
+    }
+
+    public static CustomPageResponse<OrderGetBetweenDatesResponse> toGetBetweenDatesResponses(Page<OrderDTO> sources) {
+        return CustomPageResponse.of(sources.map(OrderMapper::toGetBetweenDatesResponse));
+    }
 
 
 
---
+++ src/main/java/com/emirhanarici/bookingproject/payload/response/order/OrderGetBetweenDatesResponse.java
@@ -0,0 +1,21 @@
+package com.emirhanarici.bookingproject.payload.response.order;
+
+import com.emirhanarici.bookingproject.dto.OrderItemDTO;
+import com.emirhanarici.bookingproject.dto.UserDTO;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderGetBetweenDatesResponse {
+
+    private Long id;
+    private UserDTO user;
+    private LocalDateTime createdAt;
+    private List<OrderItemDTO> orderItems;
+}
