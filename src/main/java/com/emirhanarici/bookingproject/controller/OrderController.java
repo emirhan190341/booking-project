18a37b0eb2ff28d9fcd47b27097a5d71f5c90a89
emirhanaricii <emirhan190341@gmail.com>
2023-12-23 16:38:36 +0300

    Revise OrderController

--- src/main/java/com/emirhanarici/bookingproject/controller/OrderController.java
+++ src/main/java/com/emirhanarici/bookingproject/controller/OrderController.java
@@ -3,12 +3,15 @@ package com.emirhanarici.bookingproject.controller;
 import com.emirhanarici.bookingproject.dto.OrderDTO;
 import com.emirhanarici.bookingproject.model.mapper.order.OrderMapper;
 import com.emirhanarici.bookingproject.payload.request.order.CreateOrderRequest;
+import com.emirhanarici.bookingproject.payload.request.pagination.PaginatedFindAllRequest;
+import com.emirhanarici.bookingproject.payload.request.pagination.PaginationRequest;
 import com.emirhanarici.bookingproject.payload.response.CustomResponse;
 import com.emirhanarici.bookingproject.payload.response.order.OrderCreatedResponse;
 import com.emirhanarici.bookingproject.payload.response.order.OrderGetResponse;
 import com.emirhanarici.bookingproject.service.OrderSaveService;
 import com.emirhanarici.bookingproject.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +40,36 @@ public class OrderController {
 
         return CustomResponse.ok(response);
     }
+
+    @GetMapping("/customer/{customerId}")
+    public CustomResponse<CustomPageResponse<OrderGetByCustomerResponse>> getOrdersByCustomerId(
+            @PathVariable Long customerId,
+            @RequestBody PaginationRequest paginationRequest
+    ) {
+
+        final Page<OrderDTO> pageOfOrderDTOs = orderService
+                .findAllOrdersByCustomerId(customerId, paginationRequest);
+
+        final CustomPageResponse<OrderGetByCustomerResponse> response = OrderMapper
+                .toGetByCustomerResponse(pageOfOrderDTOs);
+        return CustomResponse.ok(response);
+
+    }
+
+    @PostMapping("/between-dates")
+    public CustomResponse<CustomPageResponse<OrderGetBetweenDatesResponse>> getOrdersBetweenTwoDates(
+            @RequestBody PaginatedFindAllRequest paginatedFindAllRequest
+    ) {
+        final Page<OrderDTO> pageOfOrderDTOs = orderService
+                .findAllOrdersBetweenTwoDatesAndPagination(paginatedFindAllRequest);
+        final CustomPageResponse<OrderGetBetweenDatesResponse> response = OrderMapper
+                .toGetBetweenDatesResponses(pageOfOrderDTOs);
+
+        return CustomResponse.ok(response);
+    }
+
+
+
 
 
 }
