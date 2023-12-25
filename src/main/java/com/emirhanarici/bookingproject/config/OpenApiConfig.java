5ee410e281aa10957c8507a604a9c890828ad086
emirhanaricii <emirhan190341@gmail.com>
2023-12-25 16:56:40 +0300

    OpenApiConfig added

---
+++ src/main/java/com/emirhanarici/bookingproject/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.emirhanarici.bookingproject.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Emirhan Arici",
+                        url = "https://github.com/emirhan190341/booking-project/"
+                ),
+                description = "Booking Project " +
+                        "(Spring Boot, Spring Security , PostgreSql, JUnit, Integration Test, Docker, Test Container) ",
+                title = "Booking Project App API",
+                version = "1.0.0"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
